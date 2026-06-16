419b5a421240a18c6c9181d0fe9917c72ad39c1d
Rupayon Haldar <80724680+rupayon123@users.noreply.github.com>
2026-06-16 16:10:45 -0400

    Tighten __proto__ regression assertion

--- src/proto.test.ts
+++ src/proto.test.ts
@@ -4,8 +4,13 @@ import { parse } from "./index";
 describe("object key parsing", () => {
     it("preserves __proto__ as an own enumerable key", () => {
         const value = parse('{"__proto__":0}');
+        const descriptor = Object.getOwnPropertyDescriptor(value, "__proto__");
 
-        expect(value).toEqual({ __proto__: 0 });
-        expect(Object.prototype.hasOwnProperty.call(value, "__proto__")).toBe(true);
+        expect(descriptor).toMatchObject({
+            value: 0,
+            writable: true,
+            enumerable: true,
+            configurable: true,
+        });
     });
 });
